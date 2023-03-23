848b105023f30f3b3a66c5242cf1fa925431d759
bryanevan <35902530+bryanevan@users.noreply.github.com>
2023-03-22 15:16:40 -1000

    Update index.js

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ app.get('/users', passport.authenticate('jwt',{session:false}), (req, res) => {
     });
 });
 
-app.get('/movies', passport.authenticate('jwt',{session:false}), (req, res) => {
+app.get('/movies', (req, res) => {
   Movies.find({ Movies: req.params.Movies })
     .then((movies) => {
       res.json(movies);

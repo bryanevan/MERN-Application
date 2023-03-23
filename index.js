67788d1eb0339590fb3702bc25e13e875c9982c2
bryanevan <35902530+bryanevan@users.noreply.github.com>
2023-03-23 13:58:19 -1000

    updating CORS origins for front end

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const cors = require('cors');
 let allowedOrigins = [
   'http://localhost:8080',
   'http://localhost:27017',
-  'http://localhost:1234/',
+  'http://localhost:1234',
   'https://mycinema.herokuapp.com/'];
 
 app.use(cors({

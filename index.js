36cedf5a2c1fd30f2efb8ca37f08950fb7d81172
bryanevan <35902530+bryanevan@users.noreply.github.com>
2023-03-23 14:12:49 -1000

    _
    _

--- index.js
+++ index.js
@@ -28,22 +28,22 @@ const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), { 
 
 //CORS
 const cors = require('cors');
-let allowedOrigins = [
-  'http://localhost:8080',
-  'http://localhost:27017',
-  'http://localhost:1234',
-  'https://mycinema.herokuapp.com/'];
+// let allowedOrigins = [
+//   'http://localhost:8080',
+//   'http://localhost:27017',
+//   'http://localhost:1234',
+//   'https://mycinema.herokuapp.com/'];
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application doesn/t allow access from origin ' + origin;
-      return callback(new Error(message ), false);
-    }
-    return callback(null, true);
-  }
-}));
+app.use(cors());
+//   origin: (origin, callback) => {
+//     if(!origin) return callback(null, true);
+//     if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+//       let message = 'The CORS policy for this application doesn/t allow access from origin ' + origin;
+//       return callback(new Error(message ), false);
+//     }
+//     return callback(null, true);
+//   }
+// }));
 
 //Middleware
 app.use(express.static('public')); //server static files

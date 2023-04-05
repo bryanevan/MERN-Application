0df4c4e069bc041c7216468ced5936057313565e
bryanevan <35902530+bryanevan@users.noreply.github.com>
2023-04-04 22:12:11 -1000

    Update to movies_db.js
    updated database to increase movie selection

--- db/movies_db.js
+++ db/movies_db.js
@@ -1,5 +1,4 @@
 
-
 let movie1 = {
     Title: "The Lord of the Rings: The Return of the King", 
     Description: "Continuing the plot of the previous film, Frodo, Sam and Gollum are making their final way toward Mount Doom in Mordor in order to destroy the One Ring, unaware of true intentions, while Merry, Pippin, Gandalf, Aragorn, Legolas, Gimli and the rest are joining forces together against Sauron and his legions in Minas Tirith.",
@@ -155,7 +154,7 @@ let movie8 = {
 
 let movie9 = {
     Title: "Star Wars", 
-    Description: "",
+    Description: "Amid a galactic civil war, Rebel Alliance spies have stolen plans to the Galactic Empire's Death Star, a massive space station capable of destroying entire planets. Imperial Senator Princess Leia Organa of Alderaan, secretly one of the Rebellion's leaders, has obtained its schematics, but her starship is intercepted by an Imperial Star Destroyer under the command of the ruthless Darth Vader. Before she is captured, Leia hides the plans in the memory system of astromech droid R2-D2, who flees in an escape pod to the nearby desert planet Tatooine alongside his companion, protocol droid C-3PO.",
     Genre: {
         Name: "science fiction",
         Description: "Science fiction (or sci-fi) is a film genre that uses speculative, fictional science-based depictions of phenomena that are not fully accepted by mainstream science, such as extraterrestrial lifeforms, spacecraft, robots, cyborgs, dinosaurs, interstellar travel, time travel, or other technologies.",
@@ -171,7 +170,6 @@ let movie9 = {
     year: "1977",
     featured: "yes",
 }
-
 
 let movie10 = {
     Title: "Avatar: The Way of Water", 
@@ -192,4 +190,100 @@ let movie10 = {
     featured: "yes",
 }
 
-db.movies.insertMany([movie1, movie2, movie3, movie4, movie5, movie6, movie7, movie8, movie9, movie10]);
+let movie11 = {
+    Title: "Pulp Fiction", 
+    Description: "The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption.",
+    Genre: {
+        Name: "action",
+        Description: "Action film is a film genre in which the protagonist is thrust into a series of events that typically involve violence and physical feats.",
+    },
+    Director: 
+    {
+        Name: "Quentin Tarantino", 
+        bio: "Quentin Tarantino was born on March 27, 1963, in Knoxville, Tennessee. He is the only child of Connie McHugh, who is part Cherokee and part Irish, and actor Tony Tarantino, who left the family before Quentin was born. Moving to California at the age of 4, Tarantino developed his love for movies at an early age.",
+        Birthyear: "1963",
+        Deathyear: "present",
+    },
+    imageUrl: "https://upload.wikimedia.org/wikipedia/en/3/3b/Pulp_Fiction_%281994%29_poster.jpg",
+    year: "1994",
+    featured: "yes",
+}
+
+let movie12 = {
+    Title: "Inglorious Basterds", 
+    Description: "The film tells an alternate history story of two plots to assassinate Nazi Germany's leadership—one planned by Shosanna Dreyfus, a young French Jewish cinema proprietor, and the other by the British; but is ultimately conducted solely by a team of Jewish American soldiers led by First Lieutenant Aldo Raine.",
+    Genre: {
+        Name: "action",
+        Description: "Action film is a film genre in which the protagonist is thrust into a series of events that typically involve violence and physical feats.",
+    },
+    Director: 
+    {
+        Name: "Quentin Tarantino", 
+        bio: "Quentin Tarantino was born on March 27, 1963, in Knoxville, Tennessee. He is the only child of Connie McHugh, who is part Cherokee and part Irish, and actor Tony Tarantino, who left the family before Quentin was born. Moving to California at the age of 4, Tarantino developed his love for movies at an early age.",
+        Birthyear: "1963",
+        Deathyear: "present",
+    },
+    imageUrl: "https://upload.wikimedia.org/wikipedia/en/c/c3/Inglourious_Basterds_poster.jpg",
+    year: "2009",
+    featured: "yes",
+}
+
+let movie13 = {
+    Title: "Star Wars: The Rise of Skywalker", 
+    Description: "The Rise of Skywalker follows Rey, Finn, and Poe Dameron as they lead the Resistance's final stand against Supreme Leader Kylo Ren and the First Order, who are aided by the return of the Galactic Emperor, Palpatine. Lucasfilm Ltd.",
+    Genre: {
+        Name: "science fiction",
+        Description: "Science fiction (or sci-fi) is a film genre that uses speculative, fictional science-based depictions of phenomena that are not fully accepted by mainstream science, such as extraterrestrial lifeforms, spacecraft, robots, cyborgs, dinosaurs, interstellar travel, time travel, or other technologies.",
+    },
+    Director: 
+    {
+        Name: "George Lucas",
+        bio: "George Walton Lucas Jr. is an American filmmaker. Lucas is best known for creating the Star Wars and Indiana Jones franchises and founding Lucasfilm, LucasArts, Industrial Light & Magic and THX.",
+        Birthyear: "1944",
+        Deathyear: "present",
+    },
+    imageUrl: "https://upload.wikimedia.org/wikipedia/en/a/af/Star_Wars_The_Rise_of_Skywalker_poster.jpg",
+    year: "2019",
+    featured: "yes",
+}
+
+let movie14 = {
+    Title: "The Wizard of Oz", 
+    Description: "Young Dorothy Gale and her dog Toto are swept away by a tornado from their Kansas farm to the magical Land of Oz, and embark on a quest with three new friends to see the Wizard, who can return her to her home and fulfill the others' wishes.",
+    Genre: {
+        Name: "fantasy",
+        Description: "Fantasy films are films that belong to the fantasy genre with fantastic themes, usually magic, supernatural events, mythology, folklore, or exotic fantasy worlds.",
+    },
+    Director: 
+    { 
+        Name: "Victor Fleming",
+        bio: "Victor Lonzo Fleming was an American film director, cinematographer, and producer.",
+        Birthyear: "1889",
+        Deathyear: "1949",
+    },
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Wizard_of_oz_movie_poster.jpg/401px-Wizard_of_oz_movie_poster.jpg",
+    year:"1939",
+    featured: "yes",
+}
+
+let movie15 = {
+    Title: "Fight Club", 
+    Description: "It is based on the 1996 novel of the same name by Chuck Palahniuk. Norton plays the unnamed narrator, who is discontented with his white-collar job. He forms a "fight club" with soap salesman Tyler Durden (Pitt), and becomes embroiled in a relationship with a mysterious woman, Marla Singer (Bonham Carter).",
+    Genre: {
+        Name: "thriller",
+        Description: "Thriller is a genre of fiction with numerous, often overlapping, subgenres, including crime, horror and detective fiction.",
+    },
+    Director: 
+    {
+        Name: "David Fincher",
+        bio: "David Andrew Leo Fincher is an American film director. His films, mostly psychological thrillers, have received 40 nominations at the Academy Awards, including three for him as Best Director.",
+        Birthyear: "1962",
+        Deathyear: "present",
+    },
+    imageUrl: "https://upload.wikimedia.org/wikipedia/en/f/fc/Fight_Club_poster.jpg",
+    year: "1999",
+    featured: "yes",
+}
+
+db.movies.insertMany([movie1, movie2, movie3, movie4, movie5, movie6, movie7, movie8, movie9, movie10,
+movie11, movie12, movie13, movie14, movie15]);

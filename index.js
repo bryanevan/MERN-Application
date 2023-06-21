c95f449ff6e47ce9be2eeb99c72510824598551a
bryanevan <35902530+bryanevan@users.noreply.github.com>
2023-06-20 15:48:45 -1000

    Add origin access for Angular Version
    adding port 4200 access for ng server

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ let allowedOrigins = [
   'http://localhost:1234',
   'http://localhost:52090',
   'http://localhost:64003',
+  'http://localhost:4200',
   'https://mycinema.herokuapp.com/',
   'https://my-cinema808.netlify.app'];
 
